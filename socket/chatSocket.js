14072506b17d6e1508c44e09105dce0689905af9
PriyankaGarg2 <pg.24july@gmail.com>
2016-09-27 17:04:37 +0530

    Implemented one to one chat

--- socket/chatSocket.js
+++ socket/chatSocket.js
@@ -3,6 +3,7 @@ module.exports=function(ioConn) {
   ioConn.on('connection' , function(socket){
     console.log("User connected");
 
+    //user gets disconnected
     socket.on('disconnect', function(){
       var name=socket.name;
       for(var i=0;i<arr.length;i++){
@@ -11,16 +12,18 @@ module.exports=function(ioConn) {
         }
       }
       arr.splice(i,1);
-      //socket.emit('logarr',arr);
+      socket.broadcast.emit('logarr',arr);
+      socket.emit('logarr',arr);
       console.log("User disconnected");
     });
 
+    //message gets displayed
     socket.on('message', function(msg){
       console.log('message: ' + msg);
-      socket.broadcast.emit('receiveMsg',msg);
+      socket.to(msg.sendTo).emit('receiveMsg',msg.message);
     });
 
-    //online users
+    //online users-user gets connected
     socket.on('logged',function(username){
       var f=0;
       for(var i=0;i<arr.length;i++){
@@ -33,11 +36,27 @@ module.exports=function(ioConn) {
         var obj={}
         obj.name=username;
         obj.id=socket.id;
+        //obj.socket=socket;
         arr.push(obj);
         socket.name=username;
+        socket.broadcast.emit('logarr',arr);
+        socket.emit('logarr',arr);
       }
-      socket.emit('logarr',arr);
+
     });
+
+    //receiving sender and receiver id
+    socket.on('chat-start',function(msg){
+      socket.to(msg.receiverId).emit('chatReq',{'senderId':msg.senderId,'senderName':msg.senderName,'receiverId':msg.receiverId});
+
+    });
+
+    //accept request
+    socket.on("acceptedRequest",function(msg){
+      //console.log("inside acceptedRequest2..cor "+ msg.receiverId);
+      socket.to(msg.senderId).emit("acceptedReq",{"receiverId":msg.receiverId,"senderId":msg.senderId});
+    });
+
 
   });
 
